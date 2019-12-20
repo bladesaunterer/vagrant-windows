dc0c478e456f62385adc514c6e9e8d762f63e46e
Carson Lourenco <carsonl@orionhealth.com>
2019-12-20 15:46:42 +1300

    Set up default applications

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,33 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "gusztavvargadr/windows-10"
-  if Vagrant.has_plugin?("vagrant-vbguest") then
-    config.vbguest.auto_update = false
+
+  config.vm.network "forwarded_port", guest: 3041, host: 9001
+  config.vm.network "forwarded_port", guest: 4031, host: 9002
+  config.vm.network "forwarded_port", guest: 8081, host: 9003
+  config.vm.network "forwarded_port", guest: 8444, host: 9004
+  config.vm.network "forwarded_port", guest: 8449, host: 9005
+
+  config.vm.provider "virtualbox" do |vb|
+   vb.name = "Windows 10 VM"
+   vb.customize ["modifyvm", :id, "--vram", "128"]
+   vb.customize ['modifyvm', :id, '--clipboard', 'bidirectional'] 
+   vb.gui = true
   end
+
+  config.vm.provision "shell", privileged: "true", inline: <<-'POWERSHELL'
+    # Install Boxstarter
+    . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
+
+    # Copy setup.ps1 to the Temp directory and then run boxstarter with our setup.ps1 script
+    $env:PSModulePath = "$([System.Environment]::GetEnvironmentVariable('PSModulePath', 'User'));$([System.Environment]::GetEnvironmentVariable('PSModulePath', 'Machine'))"
+    cp C:\vagrant\setup.ps1 $env:TEMP
+    Import-Module Boxstarter.Chocolatey
+    $credential = New-Object System.Management.Automation.PSCredential("vagrant", (ConvertTo-SecureString "vagrant" -AsPlainText -Force))
+    Install-BoxstarterPackage $env:TEMP\setup.ps1 -Credential $credential
+
+  POWERSHELL
+
 end
+
+
+  
---
+++ setup.ps1
@@ -0,0 +1,47 @@
+# Boxstarter setup script
+ 
+# Notes:
+#  - This file has to be idempotent. it will be run several times if the
+#    computer needs to be restarted. When that happens, Boxstarter schedules
+#    this script to run again with an auto-logon. Fortunately choco install
+#    handles trying to install the same package more than once.
+#  - Pass -y to choco install to avoid interactive prompts
+ 
+ 
+# Fix Windows Explorer
+Set-WindowsExplorerOptions -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+ 
+# Useful apps
+choco install -y googlechrome
+choco install -y firefox
+choco install -y 7zip
+choco install -y notepadplusplus
+choco install git -y -params '"/GitAndUnixToolsOnPath /NoAutoCrlf"'
+choco install -y sql-server-management-studio
+ 
+C:\vagrant\RhapsodyIDE6.4.2.exe /S
+
+Remove-Item 'C:\Users\Public\Desktop\Boxstarter Shell.lnk'
+
+$Desktop = [Environment]::GetFolderPath("Desktop")
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+
+$Shortcut = "${Desktop}\Notepad++.lnk"
+$Shortcut.TargetPath = $WScriptShell.CreateShortcut("${env:ProgramFiles}\Notepad++\notepad++.exe")
+$Shortcut.Save()
+
+$Shortcut = "${Desktop}\Rhapsody.lnk"
+$Shortcut.TargetPath = $WScriptShell.CreateShortcut("${env:ProgramFiles(x86)}\Orion Health\Rhapsody IDE 6\Rhapsody IDE\RhapAdmin6.exe")
+$Shortcut.Save()
+
+$Shortcut = "${Desktop}\SQL Server Management Studio 18.lnk"
+$Shortcut.TargetPath = $WScriptShell.CreateShortcut("${env:ProgramFiles(x86)}\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe")
+$Shortcut.Save()
+
+$Shortcut = "${Desktop}\Git Bash.lnk"
+$Shortcut.TargetPath = $WScriptShell.CreateShortcut("${env:ProgramFiles}\Git\git-bash.exe")
+$Shortcut.Arguments = "--cd-to-home"
+$Shortcut.Save()
+
+exit
